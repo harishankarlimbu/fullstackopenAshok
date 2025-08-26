d06db553b704f3b05634db7bc818d7047bfea30b
Hari Shankar Limbu <limashok212@gmail.com>
2025-08-26 21:22:25 +0545

    update: 4.7 Done

---
+++ Part-4/bloglist/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const { mongoUrl } = require('./utils/config')
+const Blog = require('./models/blog')
+
+const app = express()
+mongoose.connect(mongoUrl)
+  .then(() => {
+    console.log('Connected to MongoDB')
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error.message)
+  })
+
+app.use(express.json())
+
+app.get('/api/blogs', async (request, response) => {
+  const blogs = await Blog.find({})
+  response.json(blogs)
+})
+
+app.post('/api/blogs', async (request, response) => {
+  const blog = new Blog(request.body)
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
+})
+
+module.exports = app
--- part3/phonebook/Backend/nodemon.js
+++
@@ -1,5 +0,0 @@
-{
-  "watch": ["index.js", "models", "routes"],
-  "ext": "js,json",
-  "ignore": ["dist", "node_modules"]
-}
