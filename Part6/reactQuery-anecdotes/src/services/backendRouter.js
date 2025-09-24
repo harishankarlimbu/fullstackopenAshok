686da43fb30df2fa71544c578bdc8545a0930d27
Hari Shankar Limbu <limashok212@gmail.com>
2025-09-24 20:55:55 +0545

    6.20 Done

--- Part6/reactQuery-anecdotes/src/services/backendRouter.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const baseUrl = 'http://localhost:5000/anecdotes'
-export const getAnecdotes = async () => {
-  const response = await axios.get(baseUrl)
-  return response.data
-}   
